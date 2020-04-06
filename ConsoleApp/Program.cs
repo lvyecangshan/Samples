a3760a8217fb8214832ac358d078463538f8c2f4
yanhy <yanhongye@yeah.net>
2020-04-06 19:42:37 +0800

    平台调用

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using MathLib1;
@@ -11,11 +12,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            MyMathWrapper1 m = new MyMathWrapper1();
+            MyMathWrapper1 m1 = new MyMathWrapper1();
 
-            Console.WriteLine(m.Add(1, 2));
+            Console.WriteLine(m1.Add(1, 2));
+
+            Console.WriteLine(Add(1, 2));
 
             Console.Read();
         }
+
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Add(int a, int b);
     }
 }
