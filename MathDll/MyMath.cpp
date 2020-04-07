0ad6a3e1556da5656b41fbcb568cce4581c70b2c
yanhy <yanhongye@yeah.net>
2020-04-07 15:43:08 +0800

    NuGet测试

--- MathDll/MyMath.cpp
+++ MathDll/MyMath.cpp
@@ -1,7 +1,16 @@
 #include "pch.h"
 #include "MyMath.h"
+#include<boost/container/list.hpp>
 
 int MyMath::Add(int a, int b)
 {
+	using namespace boost::container;
+
+	auto *l = new list<int>();
+
+	l->push_back(1);
+
+	delete l;
+
 	return a + b;
 }
