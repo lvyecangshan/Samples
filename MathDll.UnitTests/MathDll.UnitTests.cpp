9fb12cd64dffc8ecc3fcf38aaae700a1b5ba5c40
yanhongye <yanhongye@hymson.com>
2020-04-15 17:57:55 +0800

    添加单元测试项目

---
+++ MathDll.UnitTests/MathDll.UnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MathDllUnitTests
+{
+	TEST_CLASS(MathDllUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
