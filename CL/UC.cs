908aac4c7f207b6f296c3d2f1837483bc558b7ac
yanhongye <yanhongye@hymson.com>
2020-04-29 16:06:36 +0800

    添加羡慕

---
+++ CL/UC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CL
+{
+    public partial class UC : UserControl
+    {
+        public UC()
+        {
+            InitializeComponent();
+        }
+
+        [Browsable(true)]
+        public string UCName
+        {
+            get
+            {
+                return ucName;
+            }
+            set
+            {
+                ucName = value;
+
+                this.OnMouseClick(null);
+            }
+        }
+
+        private void UC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string ucName;
+    }
+}
